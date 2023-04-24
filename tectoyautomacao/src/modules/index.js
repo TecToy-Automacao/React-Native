b4dcdaba05b127b46bcd58f7b6d91a7de5bad195
Daniel Simões de Almeida <daniel@projetoacbr.com.br>
2023-04-24 14:09:19 -0300

    Removendo fontes antigos e sem manutenção

--- tectoyautomacao/src/modules/index.js
+++
@@ -1,5 +0,0 @@
-import * as TectoySunmiSdkModule from './TectoySunmiSdkModule';
-import * as LampadaSdkModule from './LampadaSdkModule';
-import * as MsitefSdkModule from './MsitefSdkModule';
-
-export{ TectoySunmiSdkModule, LampadaSdkModule, MsitefSdkModule }
